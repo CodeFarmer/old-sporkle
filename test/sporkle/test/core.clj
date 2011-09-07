a0c1bede97ecb4e0bdffecc070e62dcccdc81233
CodeFarmer <joel.gluth@gmail.com>
2011-09-07 11:28:18 +0100

    First attempt to use lein test, seems to Just Work.

--- test/sporkle/test/core.clj
+++ test/sporkle/test/core.clj
@@ -1,6 +1,9 @@
 (ns sporkle.test.core
   (:use [sporkle.core])
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:require [clojure.java.io :as io]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest test-byte-stream-seq ;; FIXME: write
+  (let [cs (byte-stream-seq (io/input-stream "test/fixtures/Nothing.class"))]
+    (is (= 0xCA (first cs)) "We should be able to get a single byte from the stream")
+    (is (= '(0xCA 0xFE) (take 2 cs)) "Having taken a byte, we should be able to take it again (ie., behave like a seq and not a stream")))
