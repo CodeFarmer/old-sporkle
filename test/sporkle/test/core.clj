78a26d73d7e5c3208e283b44166042b1e3e4c8a7
CodeFarmer <joel.gluth@gmail.com>
2011-09-07 11:52:52 +0100

    Discovered (testing "thing" ...) macro, yay

--- test/sporkle/test/core.clj
+++ test/sporkle/test/core.clj
@@ -3,7 +3,10 @@
   (:use [clojure.test])
   (:require [clojure.java.io :as io]))
 
-(deftest test-byte-stream-seq ;; FIXME: write
-  (let [cs (byte-stream-seq (io/input-stream "test/fixtures/Nothing.class"))]
-    (is (= 0xCA (first cs)) "We should be able to get a single byte from the stream")
-    (is (= '(0xCA 0xFE) (take 2 cs)) "Having taken a byte, we should be able to take it again (ie., behave like a seq and not a stream")))
+(deftest test-byte-stream-seq
+  (testing "byte-stream-seq"
+    (let [cs (byte-stream-seq (io/input-stream "test/fixtures/Nothing.class"))]
+      (testing "getting a single byte"
+        (is (= 0xCA (first cs)) "we should be able to get a byte from the stream"))
+      (testing "getting bytes after some have been taken"
+        (is (= '(0xCA 0xFE) (take 2 cs)) "we should be able to retake bytes (ie., behave like a seq and not a stream")))))
