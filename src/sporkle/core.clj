edd3ab116a6c47879b7b3df4a0c385f455898b8d
CodeFarmer <joel.gluth@gmail.com>
2011-09-07 12:38:39 +0100

    The wonders of unit testing; terminate the seq when the stream does :P

--- src/sporkle/core.clj
+++ src/sporkle/core.clj
@@ -3,5 +3,7 @@
 (defn byte-stream-seq
   "Returns a lazy sequence of bytes from a java.io.InputStream"
   [^java.io.InputStream stream]
-  (when-let [b (.read stream)]
-    (cons b (lazy-seq (byte-stream-seq stream)))))
+  (let [b (.read stream)]
+    (if (= b -1)
+      ()
+      (cons b (lazy-seq (byte-stream-seq stream))))))
--- test/sporkle/test/core.clj
+++ test/sporkle/test/core.clj
@@ -4,9 +4,10 @@
   (:require [clojure.java.io :as io]))
 
 (deftest test-byte-stream-seq
-  (testing "byte-stream-seq"
+  (testing "byte-stream-seq's seqiness"
     (let [cs (byte-stream-seq (io/input-stream "test/fixtures/Nothing.class"))]
-      (testing "getting a single byte"
-        (is (= 0xCA (first cs)) "we should be able to get a byte from the stream"))
-      (testing "getting bytes after some have been taken"
+      (testing "when the seq is new"
+        (is (= 0xCA (first cs)) "we should be able to get a byte from the stream")
+        (is (= 188 (count cs)) "the seq should return the byte count of the stream"))
+      (testing "after some bytes have been taken"
         (is (= '(0xCA 0xFE) (take 2 cs)) "we should be able to retake bytes (ie., behave like a seq and not a stream")))))
