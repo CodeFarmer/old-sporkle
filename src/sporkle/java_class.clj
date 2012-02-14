cb581a4daaea1bd28ed6ce1743c21cf8b8ccac8c
CodeFarmer <joel.gluth@gmail.com>
2012-02-14 17:36:44 +0000

    More failing tests to remind me what to do next

--- src/sporkle/java_class.clj
+++ src/sporkle/java_class.clj
@@ -1,4 +1,21 @@
-(ns sporkle.java-class)
+(ns sporkle.java-class
+  (:use [sporkle.core]))
 
 (defn java-class [class-name]
-  {:constant-pool []})
+  {:magic         [0xCA 0xFE 0xBA 0xBE]
+   :constant-pool []
+   :methods       []
+   :this-class    [0x00 0x01]})
+
+
+(defn cp-find [constant-pool value-map]
+  "Find the cp-index into constant pool where a constant with particular contents can be found"
+  
+  (let [indexed-cp (each-with-index constant-pool)]
+
+    ))
+
+;; cp methods, consider consilidating after you know what shape they are
+(defn cp-add-utf8 [constant-pool string]
+  "Given a constant pool and a string, return a vector containing the constant pool with the Utf8 constant, and the cp index"
+  [constant-pool (count constant-pool)])
--- test/sporkle/test/java_class.clj
+++ test/sporkle/test/java_class.clj
@@ -1,18 +1,44 @@
 (ns sporkle.test.java-class
+  
   (:use [clojure.test])
+  (:use [sporkle.core])
   (:use [sporkle.java-class])
-  (:use [sporkle.classfile]))
+  (:use [sporkle.classfile])
+  (:require [clojure.java.io :as io]))
 
 (deftest test-minimal-class
-  (testing "The creation of an empty class, with default everything"
+  (testing "that the creation of a minimal class should fill out the required keys in the class-file struct"
 
     (let [clazz (java-class "Nothing")]
       (is (map? clazz)
           "java-class should return a Map")
+      
       (is (vector? (:constant-pool clazz))
           "java-class should have a vector constant pool")
-      (is (not (nil? (:this-class clazz)))
-          "java-class should have a this-class member")
-      (is (= "Nothing" (class-name clazz))
-          "this-class member should resolve to a name"))))
 
+      (is (vector? (:methods clazz))
+          "java-class should have a methods vector")
+      
+      (is (not (nil? (:this-class clazz)))
+          "java-class should have a this-class member"))))
+
+(deftest test-cp-find
+  (testing "With s real class constant pool"
+    (let [constant-pool (:constant-pool (read-java-class (byte-stream-seq (io/input-stream "test/fixtures/Nothing.class"))))]
+
+      (is (= 12 (cp-find constant-pool {:tag CONSTANT_Utf8 :bytes (seq (.getBytes "Nothing"))}))
+          "The string 'Nothing' should be stored in a UTf8 constant"))))
+
+(deftest test-include-constant
+  (testing "with an empty constant pool")
+
+  (testing "with some entries already in the pool"))
+
+
+(deftest test-minimal-meaningful-class
+  (testing "that the creation of a minimal class should have minimal, self-consistent fields")
+
+  (let [clazz (java-class "Nothing")]
+
+    (comment (is (= "Nothing" (class-name clazz)
+                    "this-class member should correctly resolve to the name")))))
