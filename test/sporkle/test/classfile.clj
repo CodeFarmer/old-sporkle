c77915a03e884ba2e1e7a344eeb924358ad57759
CodeFarmer <joel.gluth@gmail.com>
2012-02-17 15:04:56 +0000

    Test gardening, mostly for readability

--- test/sporkle/test/classfile.clj
+++ test/sporkle/test/classfile.clj
@@ -28,48 +28,70 @@
 
     (let [[entry rest] (read-constant-pool-entry [0x03 0x00 0x10 0x01 0x01 0xFF])]
       
-      (is (= [0xFF] rest)                           "should correctly return the remaining bytes")
-      (is (= 0x00100101 (constant-value nil entry)) "should the two bytes into an integer")))
+      (is (= [0xFF] rest)
+          "should correctly return the remaining bytes")
+      (is (= 0x00100101 (constant-value nil entry))
+          "should the two bytes into an integer")))
 
   (testing "reading a method ref entry, with a trailing byte"
 
-    (let [[entry rest] (read-constant-pool-entry [0x0A 0x00 0x03 0x00 0x0A])]
-      (is (= CONSTANT_Methodref (tag entry)) "should record the correct tag")
-      (is (= [0x00 0x03] (:class-index entry)) "should record the class index bytes")
-      (comment "FIXME implement more tests for method-ref"))
+    (let [[entry rest] (read-constant-pool-entry [0x0A 0x00 0x03 0x00 0x0A 0x01])]
+      (is (= CONSTANT_Methodref (tag entry))
+          "should record the correct tag")
+      (is (= [0x00 0x03] (:class-index entry))
+          "should record the class index bytes")
+      (is (= [0x00 0x0A] (:name-and-type-index entry))
+          "should record the name and type index")
+      (is (= [0x01] rest)
+          "should leave the remainder of the stream correctly"))
 
   (testing "reading a field ref entry, with a trailing byte"
 
     (let [[entry rest] (read-constant-pool-entry [0x09 0x00 0x03 0x00 0x0A])]
-      (is (= CONSTANT_Fieldref (tag entry)) "should record the correct tag")
-      (is (= [0x00 0x03] (:class-index entry)) "should record the class index bytes")
-      (is (= [0x00 0x0A] (:name-and-type-index entry "should record the name and type index bytes"))))
+      (is (= CONSTANT_Fieldref (tag entry))
+          "should record the correct tag")
+      (is (= [0x00 0x03] (:class-index entry))
+          "should record the class index bytes")
+      (is (= [0x00 0x0A] (:name-and-type-index entry))
+          "should record the name and type index bytes"))
 
     (testing "reading an interface method ref entry, with a trailing byte"
 
       (let [[entry rest] (read-constant-pool-entry [0x0B 0x00 0x03 0x00 0x0A])]
-        (is (= CONSTANT_InterfaceMethodref (tag entry)) "should record the correct tag")
-        (is (= [0x00 0x03] (:class-index entry)) "should record the class index bytes")
-        (is (= [0x00 0x0A] (:name-and-type-index entry "should record the name and type index bytes"))))))
+        (is (= CONSTANT_InterfaceMethodref (tag entry))
+            "should record the correct tag")
+        (is (= [0x00 0x03] (:class-index entry))
+            "should record the class index bytes")
+        (is (= [0x00 0x0A] (:name-and-type-index entry))
+            "should record the name and type index bytes"))))
 
 
   (testing "reading a string entry with trailing bytes"
     (let [[entry rest] (read-constant-pool-entry [0x08 0x00 0x05 0x10 0x00])]
-      (is (= CONSTANT_String (tag entry)) "should record the correct tag")
-      (is (= [0x00 0x05] (:string-index entry)) "should record the string-index bytes")
-      (is (= [0x10 0x00]) "should leave the correct remainder")))
+      (is (= CONSTANT_String (tag entry))
+          "should record the correct tag")
+      (is (= [0x00 0x05] (:string-index entry))
+          "should record the string-index bytes")
+      (is (= [0x10 0x00])
+          "should leave the correct remainder")))
 
   (testing "Reading a float constant, with a trailing byte"
 
     (let [[entry rest] (read-constant-pool-entry [0x04 0x40 0x20 0x00 0x00 0xFF])]
 
-      (is (= CONSTANT_Float (tag entry)) "should read the correct tag")
-      (is (= [0x40 0x20 0x00 0x00] (:bytes entry))      "should have four bytes")
-      (is (= [0xFF] rest)                "should correctly return the remaining bytes")
-      (is (= 2.5 (constant-value nil entry)) "should be able to read the bytes into a float")))
+      (is (= CONSTANT_Float (tag entry))
+          "should read the correct tag")
+      (is (= [0x40 0x20 0x00 0x00] (:bytes entry))
+          "should have four bytes")
+      (is (= [0xFF] rest)
+          "should correctly return the remaining bytes")
+      (is (= 2.5 (constant-value nil entry))
+          "should be able to read the bytes into a float")))
 
   ;; TODO
   (comment (testing "reading a long constant"))
+
+  ;; TODO
   (comment (testing "reading a double constant"))
   
   (testing "reading from a constant with an unknown tag value"
@@ -83,43 +105,54 @@
 
     (let [[constant-pool-maplet remainder] (read-constant-pool-maplet (drop 8 (byte-stream-seq (io/input-stream "test/fixtures/Nothing.class"))))]
       (let [pool (:constant-pool constant-pool-maplet)]
-        (is (= 12 (count pool)) "should read the right number of constants")
-        (is (every? #(contains? % :tag) pool) "should return a seq of objects with tag fields")
-        (is (= 0x0A (tag (first pool))) "should have the constant pool objects in the right order"))
-      ))
+        (is (= 12 (count pool))
+            "should read the right number of constants")
+        (is (every? #(contains? % :tag) pool)
+            "should return a seq of objects with tag fields")
+        (is (= 0x0A (tag (first pool)))
+            "should have the constant pool objects in the right order"))))
 
   (testing "reading the constant pool from a class with wide constants"
 
-    (let [[constant-pool-maplet remainder] (read-constant-pool-maplet (drop 8 (byte-stream-seq (io/input-stream "test/fixtures/LongFieldStaticInit.class"))))]
+    (let [[constant-pool-maplet remainder] (read-constant-pool-maplet (drop 8 (byte-stream-seq (io/input-stream "test/fixtures/LongFieldStaticInit.class"))))
+          constant-pool (:constant-pool constant-pool-maplet)]
 
-      (is (= 19 (count (:constant-pool constant-pool-maplet)))
-          "The constant pool has only 18 constants, but there should be a spacer for the float at index 3"))))
-
+      (is (= 19 (count constant-pool))
+          "The constant pool has only 18 constants, but there should be a spacer for the float")
+      (is (= [:spacer] (:tag (nth constant-pool 2)))
+          "The float spacer should be at cp-index 3"))))
 
 (deftest test-read-attribute
 
   (testing "reading a generic attribute from a byte stream with trailing bytes"
 
     (let [[attr remainder] (read-attribute [0x00 0x01 0x00 0x00 0x00 0x03 0x0A 0x0B 0x0C 0x0D])]
-      (is (= 0x0001 (:attribute-name-index attr)) "first two bytes should be the atribute name index")
-      (is (= [0x0A 0x0B 0x0C] (:info attr)) "info should contain the right three bytes")
-      (is (= [0x0D] remainder) "should correctly leave the remaining byte"))))
+      (is (= 0x0001 (:attribute-name-index attr))
+          "first two bytes should be the atribute name index")
+      (is (= [0x0A 0x0B 0x0C] (:info attr))
+          "info should contain the right three bytes")
+      (is (= [0x0D] remainder)
+          "should correctly leave the remaining byte"))))
 
 
 (deftest test-read-attributes-maplet
 
   (testing "reading an empty attribute list"
     
-    (is (= [{:attributes []} [0x01 0x02] (read-attributes-maplet [0x00 0x00 0x01 0x02])]) "should return the byte stream, minus the first two zero bytes that display the count"))
+    (is (= [{:attributes []} [0x01 0x02]
+            (read-attributes-maplet [0x00 0x00 0x01 0x02])])
+        "should return the byte stream, minus the first two zero bytes that display the count"))
   
   (testing "reading a simple attribute list, two attributes long, with some trailing bytes")
 
   (let [[attributes-maplet remainder] (read-attributes-maplet [0x00 0x02 0x00 0x01 0x00 0x00 0x00 0x03 0x0A 0x0B 0x0C 0x00 0x01 0x00 0x00 0x00 0x01 0x0A 0x0B 0x0C])]
     
     (let [attr-list (:attributes attributes-maplet)]
-      (is (= 2 (count attr-list)) "should return two attributes in the list"))
+      (is (= 2 (count attr-list))
+          "should return two attributes in the list"))
 
-    (is (= [0x0B 0x0C] remainder) "should leave the correct remainder")))
+    (is (= [0x0B 0x0C] remainder)
+        "should leave the correct remainder")))
 
 
 (deftest test-read-field-or-method-info
@@ -128,20 +161,31 @@
 
     (let [[field remainder] (read-field-or-method-info [0x00 0x01 0x00 0x04 0x00 0x05 0x00 0x00 0x00 0x01 0x00 0x06])]
 
-      (is (= [] (:attributes field)) "should have no attributes")
-      (is (= [0x00 0x01] (:access-flags field)) "should select the correct access flag bytes")
-      (is (= [0x00 0x04] (:name-index field)) "should select the correct name index bytes")
-      (is (= [0x00 0x05] (:descriptor-index field)) "should select the correct descriptor-index bytes")
-      (is (= [0x00 0x01 0x00 0x06] remainder) "Should return the correct remainder bytes")))
+      (is (= [] (:attributes field))
+          "should have no attributes")
+      (is (= [0x00 0x01] (:access-flags field))
+          "should select the correct access flag bytes")
+      (is (= [0x00 0x04] (:name-index field))
+          "should select the correct name index bytes")
+      (is (= [0x00 0x05] (:descriptor-index field))
+          "should select the correct descriptor-index bytes")
+      (is (= [0x00 0x01 0x00 0x06] remainder)
+          "Should return the correct remainder bytes")))
   
   (testing "reading a known method info with 4 trailing bytes"
     
     (let [[method remainder] (read-field-or-method-info [0x00 0x01 0x00 0x04 0x00 0x05 0x00 0x01 0x00 0x06 0x00 0x00 0x00 0x1d 0x00 0x01 0x00 0x01 0x00 0x00 0x00 0x05 0x2a 0xb7 0x00 0x01 0xb1 0x00 0x00 0x00 0x01 0x00 0x07 0x00 0x00 0x00 0x06 0x00 0x01 0x00 0x00 0x00 0x01 0x00 0x01 0x00 0x08])]
-      (is (= [0x00 0x01] (:access-flags method)) "should select the correct access flag bytes")
-      (is (= [0x00 0x04] (:name-index method)) "should select the correct name index bytes")
-      (is (= [0x00 0x05] (:descriptor-index method)) "should select the correct descriptor-index bytes")
-      (is (= 1 (count (:attributes method))) "should have one (Code) attribute")
-      (is (= [0x00 0x01 0x00 0x08] remainder) "should return the correct remainder bytes"))))
+      
+      (is (= [0x00 0x01] (:access-flags method))
+          "should select the correct access flag bytes")
+      (is (= [0x00 0x04] (:name-index method))
+          "should select the correct name index bytes")
+      (is (= [0x00 0x05] (:descriptor-index method))
+          "should select the correct descriptor-index bytes")
+      (is (= 1 (count (:attributes method)))
+          "should have one (Code) attribute")
+      (is (= [0x00 0x01 0x00 0x08] remainder)
+          "should return the correct remainder bytes"))))
 
 
 (deftest test-read-java-class
@@ -150,46 +194,58 @@
 
     (let [java-class (read-java-class-file "test/fixtures/Nothing.class")]
 
-      (is (= 0xCAFEBABE (bytes-to-unsigned-integral-type (:magic java-class))) "gotta get the magic number right")
-      (is (= 0x0000 (bytes-to-integral-type (:minor-version java-class)))   "minor version number of the class file")
-      (is (= 0x0032 (bytes-to-integral-type (:major-version java-class)))   "major version number of the class file")
-      (is (= 12 (count (:constant-pool java-class)))    "should read the correct number of constant pool entries")
+      (is (= 0xCAFEBABE (bytes-to-unsigned-integral-type (:magic java-class)))
+          "gotta get the magic number right")
+      (is (= 0x0000 (bytes-to-integral-type (:minor-version java-class)))
+          "minor version number of the class file")
+      (is (= 0x0032 (bytes-to-integral-type (:major-version java-class)))
+          "major version number of the class file")
+      (is (= 12 (count (:constant-pool java-class)))
+          "should read the correct number of constant pool entries")
       
-      (let [access-flags (access-flags java-class)]
-        (is (= ACC_SUPER access-flags) "class have no modifiers (except the fearsome ACC_SUPER)"))
-
-      (comment "These next two need to be reworked"
-               (is (= 2 (count (:this-class java-class))) "should have two bytes for its this-class constant reference")
-               (is (= 2 (count (:super-class java-class))) "should have two bytes for its superclass constant reference"))
+      (is (= ACC_SUPER (access-flags java-class))
+          "class have no modifiers (except the fearsome ACC_SUPER)")
+      
+      (is (= 2 (count (:this-class java-class)))
+          "should have two bytes for its this-class constant reference")
+      (is (= 2 (count (:super-class java-class)))
+          "should have two bytes for its superclass constant reference")
       
       ;; and the interfaces...
-      (is (= 0 (count (:interfaces java-class))) "should have no interfaces, because it's so simple")
-      (is (not (nil? (:interfaces java-class))) "should still have something in the interfaces field though")
-      (is (= 0 (count (:fields java-class))) "should have no fields")
-      (is (not (nil? (:fields java-class))) "should still have something in the fields field though")
-      (is (= 1 (count (:methods java-class))) "should have one method (<init>")
-      (is (not (nil? (:methods java-class))) "should still have something in the method fields")
+      (is (= 0 (count (:interfaces java-class)))
+          "should have no interfaces, because it's so simple")
+      (is (not (nil? (:interfaces java-class)))
+          "should still have something in the interfaces field though")
+      (is (= 0 (count (:fields java-class)))
+          "should have no fields")
+      (is (not (nil? (:fields java-class)))
+          "should still have something in the fields field though")
+      (is (= 1 (count (:methods java-class)))
+          "should have one method (<init>")
+      (is (not (nil? (:methods java-class)))
+          "should still have something in the method fields")
 
-      (is (= "Nothing" (class-name java-class)) "should be able to retrieve the class name")
-      ))
+      (is (= "Nothing" (class-name java-class))
+          "should be able to retrieve the class name")))
+
   
   (testing "reading a Java class with some marker interfaces"
 
     (let [java-class (read-java-class-file "test/fixtures/SerializableNothing.class")]
-      (= 2 (count (:interfaces java-class)) "The class should have two entries in its interface list")))
+      (= 2 (count (:interfaces java-class))
+         "The class should have two entries in its interface list")))
 
   (testing "reading a Java class with some fields"
     
     (let [java-class (read-java-class-file "test/fixtures/FieldNothing.class")]
-      (is  (= 2 (count (:fields java-class))) "The class should have two entries in its field list"))
-    
-    )
+      (is  (= 2 (count (:fields java-class)))
+           "The class should have two entries in its field list")))
 
   (testing "reading a Java class with a method"
 
     (let [java-class (read-java-class-file "test/fixtures/MethodNothing.class")]
-
-      (is (= 2 (count (:methods java-class))) "the class should return one method"))))
+      (is (= 2 (count (:methods java-class)))
+          "the class should return one method"))))
 
 ;; information about fields or methods or... anything with a :name-index
 (deftest test-get-name
