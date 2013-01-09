a6efb8c6e9517d40d6db9e9c8608e69c3696ff54
CodeFarmer <joel.gluth@gmail.com>
2013-01-09 22:16:45 +0000

    Adding failing test describing how the class creation macro should work, as Sporkle has gone back to being interesting again

---
+++ test/sporkle/test/Foo.java
@@ -0,0 +1,12 @@
+// Java file to serve as a reference for the class-macros tests
+class Foo
+{
+
+    public static final String MESSAGE = "Hello, world.";
+
+    public static void main(String[] argv)
+    {
+	System.out.println(MESSAGE);
+    }
+
+}
