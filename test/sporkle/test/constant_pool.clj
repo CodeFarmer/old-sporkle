4c7424fabe410b5fb0acdb88f36059300cd35839
CodeFarmer <joel.gluth@gmail.com>
2014-06-14 13:45:07 +0100

    Added small test for multibyte Long cp-entry-value

--- test/sporkle/test/constant_pool.clj
+++ test/sporkle/test/constant_pool.clj
@@ -11,5 +11,8 @@
           cp (:constant-pool clazz)
           long-entry (nth cp 1)]
 
-      (is (= 0x02 (cp-entry-value cp long-entry))))))
+      (is (= 0x02 (cp-entry-value cp long-entry))))
+    
+    (let [long-entry {:low-bytes [0 0 0 2], :high-bytes [0 3 0 0], :tag [5]}]
+      (is (= 0x0003000000000002 (cp-entry-value [] long-entry))))))
 
