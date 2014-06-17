4db720f8a3adfcb9d7bb7a8885ab1bfcd3b631bf
Joel Gluth <jgluth@shozilla.com>
2014-06-17 17:21:40 +0100

    Added cloverage to project; remembering to add it to profiles was getting annoying.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject sporkle "0.1.0-SNAPSHOT"
   :description "Pure-Clojure Java classfile assembler and disassembler"
   :url "http://github.com/CodeFarmer/sporkle"
+  :plugins [[lein-cloverage "1.0.0"]]
   :dependencies [[org.clojure/clojure "1.6.0"]])
