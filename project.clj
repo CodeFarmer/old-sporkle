21383e333ef3fc43f1ec7d44b52b502198b54e9c
Joel Gluth <joel.gluth@gmail.com>
2017-11-18 21:29:41 +0000

    Updated to Clojure 1.8 because apparently some tools don't like 1.3 any more

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Pure-Clojure Java classfile assembler and disassembler"
   :url "http://github.com/CodeFarmer/sporkle"
   :plugins [[lein-cloverage "1.0.0"] [jonase/eastwood "0.1.4"]]
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.14.0"]]}})
